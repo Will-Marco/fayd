7c9f74a91c023c5333a165db9cc524fa3c30799b
Will-Marco <mr.marco0101@gmail.com>
2024-09-02 10:49:00 +0500

    prepare packages for UI component and languages

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,32 @@
+import { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fayd - Fly after your dreams",
+  description: "Fly after your dreams",
+};
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body className={inter.className}>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
