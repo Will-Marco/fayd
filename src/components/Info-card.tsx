2a53353ed52a0534563dc181ef1fa5cb219d44ea
Will-Marco <mr.marco0101@gmail.com>
2024-09-02 21:18:42 +0500

    fixed

--- src/components/Info-card.tsx
+++ src/components/Info-card.tsx
@@ -6,8 +6,10 @@ export default function InfoCard({
   icon: JSX.Element;
 }) {
   return (
-    <div className="xl:w-[80%] flex max-lg:flex-col gap-2 md:gap-4 justify-center items-center max-md:border-b max-md:py-8 ">
-      <div>{icon}</div>
+    <div
+      className="xl:w-[80%] flex max-lg:flex-col gap-2 md:gap-4 justify-center items-center max-md:border-b max-md:py-8 "
+    >
+      {icon}
       <p>{text}</p>
     </div>
   );
--- src/components/views/Info.tsx
+++ src/components/views/Info.tsx
@@ -6,17 +6,17 @@ export default function Info() {
   const t = useTranslations("info");
 
   const cards = [
-    <Gem size={30} />,
-    <MessageSquare size={30} />,
-    <CreditCard size={30} />,
-    <FileText size={30} />,
+    <Gem key={1} size={40} />,
+    <MessageSquare key={2} size={40} />,
+    <CreditCard key={3} size={40} />,
+    <FileText key={4} size={35} />,
   ];
 
   return (
     <div className="bg-orange-500 px-4 md:py-8">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-6 text-center text-white">
         {cards.map((item: JSX.Element, idx: number) => (
-          <InfoCard text={t(`card${idx + 1}`)} icon={item} />
+          <InfoCard key={idx} text={t(`card${idx + 1}`)} icon={item} />
         ))}
       </div>
     </div>
